96141299515e4f5e6a7b3c718e360232b474780c
arnaud nicastro <arnaud.nicastro@gmail.com>
2021-11-17 16:11:15 +0100

    sound

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,11 @@ class ProductsController < ApplicationController
   def index
     if params[:product][:category].present?
       @products = Product.where(category: params[:product][:category])
-    elsif params[:product][:title].present?
-      @products = Product.where(category: params[:product][:title])
     else
       @products = Product.all
     end
+  end
+
   def show
     @product = Product.find(params[:id])
   end
---
+++ app/javascript/components/_clicksound.js
@@ -0,0 +1,12 @@
+function clicksound() {
+  const audio = new Audio('https://res.cloudinary.com/dmxqtfszw/video/upload/v1637151113/sound-cat_wqplvt.mp3');
+
+  const btn = document.querySelector(".sound");
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    audio.load();
+    audio.play();
+  });
+}
+
+export {clicksound}

fda05a3b3516a20f5f38d611a6626d2ad3d382b1
arnaud nicastro <arnaud.nicastro@gmail.com>
2021-11-18 13:12:50 +0100

    Edition de produit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -34,6 +34,15 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.update(product_params)
+    redirect_to product_path(@product)
+  end
+
   private
 
   def product_params

781d362b643920d2c6703217ad95d5aaef0e66e0
Romain Jumeau <romain.jumeau@hotmail.fr>
2021-11-18 16:58:51 +0100

    Search bar ok

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,24 @@
 class Product < ApplicationRecord
+  include PgSearch::Model
   belongs_to :cat
-
-  # has_one :transaction
-
   has_one_attached :image
+  AUTHORIZED_CATEGORIES = ["Hygiène 🧼", "Miam 🤤", "Jouet 🪀", "Maison 🛋"]
+  validates :category, inclusion: { in: AUTHORIZED_CATEGORIES }
+
+  # pg_search_scope :search_by_category,
+  #   against: :category
+
+  pg_search_scope :search_global,
+    against: [:title, :category],
+    associated_against: {
+      cat: :address
+    },
+    using: {
+        tsearch: { prefix: true }
+      }
+  # pg_search_scope :search_by_addrezss,
+  # against: :address,
+  # using: {
+  #     tsearch: { prefix: true }
+  #   }
 end

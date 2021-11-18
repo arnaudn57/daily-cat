408e036940842a406f2131d82f495a8b4a177d9b
arnaud nicastro <arnaud.nicastro@gmail.com>
2021-11-18 11:51:01 +0100

    sign up fix

--- app/javascript/components/_clicksound.js
+++ app/javascript/components/_clicksound.js
@@ -1,12 +1,14 @@
-function clicksound() {
+const clicksound = () => {
   const audio = new Audio('https://res.cloudinary.com/dmxqtfszw/video/upload/v1637151113/sound-cat_wqplvt.mp3');
 
   const btn = document.querySelector(".sound");
-  btn.addEventListener("click", (event) => {
-    event.preventDefault();
-    audio.load();
-    audio.play();
-  });
+  if (btn) {
+    btn.addEventListener("click", (event) => {
+      event.preventDefault();
+      audio.load();
+      audio.play();
+    });
+  }
 }
 
 export {clicksound}
